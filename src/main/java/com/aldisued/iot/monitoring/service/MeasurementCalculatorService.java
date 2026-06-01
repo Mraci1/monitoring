56802067799be696f5532d5363c376449f8afe77
MarcellVig <wmarci15@gmail.com>
2026-06-01 21:56:45 +0200

    Task 9 - completed

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -1,20 +1,33 @@
 package com.aldisued.iot.monitoring.service;
 
 
-import java.util.List;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MeasurementCalculatorService {
 
-  public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    // TODO: Task 9
-    return List.of();
-  }
+    public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
+        if (deviation > 1.0 || deviation < 0.0) {
+            throw new IllegalArgumentException("Deviation must be between 0 and 1");
+        }
+        double average = values.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
 
-  public List<Double> getMovingAverage(List<Double> data, int windowSize) {
-    // TODO: Task 10
-    return List.of();
-  }
+        double lowerBound = average * (1 - deviation);
+        double upperBound = average * (1 + deviation);
+
+        return values.stream()
+                .filter(value -> value >= lowerBound && value <= upperBound)
+                .toList();
+    }
+
+    public List<Double> getMovingAverage(List<Double> data, int windowSize) {
+        // TODO: Task 10
+        return List.of();
+    }
 
 }
