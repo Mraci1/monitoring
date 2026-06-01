3c932a6b3492105a6852cada8d0283eaeca2c9f6
MarcellVig <wmarci15@gmail.com>
2026-06-01 20:36:54 +0200

    Task 3 - completed

--- src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/SensorReadingService.java
@@ -19,8 +19,10 @@ public class SensorReadingService {
   }
 
   public SensorReading saveSensorReading(SensorReadingDto sensorReadingDto) {
-    //TODO: Task 3
-    return null;
+      SensorReading sensorReading = new SensorReading(sensorReadingDto.value(),
+              sensorReadingDto.timestamp(),
+              sensorRepository.getReferenceById(sensorReadingDto.sensorId()));
+      return sensorReadingRepository.save(sensorReading);
   }
 
 }
