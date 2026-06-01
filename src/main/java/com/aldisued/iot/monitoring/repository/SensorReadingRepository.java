920d60e7b6b550e24247b29ef2a8e31d64ca681e
MarcellVig <wmarci15@gmail.com>
2026-06-01 21:38:52 +0200

    Task 7 - completed

--- src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
+++ src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
@@ -1,7 +1,16 @@
 package com.aldisued.iot.monitoring.repository;
 
 import com.aldisued.iot.monitoring.entity.SensorReading;
+import com.aldisued.iot.monitoring.entity.SensorType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
+
+    @Query("SELECT AVG(r.value) FROM SensorReading r WHERE r.sensor.type = :type AND r.timestamp BETWEEN :from AND :to")
+    Optional<Double> findAverageValueByTypeAndTimestampBetween(SensorType type, LocalDateTime from, LocalDateTime to);
+
 }
